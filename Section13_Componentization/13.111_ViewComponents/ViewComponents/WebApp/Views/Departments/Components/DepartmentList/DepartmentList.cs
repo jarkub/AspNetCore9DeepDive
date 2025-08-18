0d768cbb61e5b5532d7754b6a7ccf78193aa9958
jaredk <jaredk@monnit.com>
2025-08-18 17:13:17 -0600

    Section 13

---
+++ Section13_Componentization/13.111_ViewComponents/ViewComponents/WebApp/Views/Departments/Components/DepartmentList/DepartmentList.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp.Models;
+
+namespace ViewComponents.Views.Departments.Components.DepartmentList
+{
+    [ViewComponent]
+    public class DepartmentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var departments = DepartmentsRepository.GetDepartments();
+            return View(departments);
+        }
+
+
+    }
+}
